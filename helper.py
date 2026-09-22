b9643608d735147292a77d301fffe5ace9481dbd
Mirza Yasir Abdullah Baig <yasirabdullah4549@gmail.com>
2026-09-22 19:25:26 +0500

    Helper File Added

---
+++ helper.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+plt.ion()
+plt.figure()
+
+
+def plot(scores, mean_scores):
+    plt.clf()
+    plt.title('Training...')
+    plt.xlabel('Number of Games')
+    plt.ylabel('Score')
+    plt.plot(scores, label='Score')
+    plt.plot(mean_scores, label='Mean Score')
+    plt.ylim(ymin=0)
+    plt.legend()
+    plt.text(len(scores) - 1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores) - 1, mean_scores[-1], str(mean_scores[-1]))
+    plt.show(block=False)
+    plt.pause(.1)
